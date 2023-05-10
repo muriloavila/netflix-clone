359587856d4f9010a88a3262ac43bbbfd825e26d
Murilo Avila <murilo.avila068@gmail.com>
2023-05-10 11:24:08 -0300

    feat: google auth and github auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,8 +3,21 @@ import Credentials from "next-auth/providers/credentials";
 import prismadb from '@/lib/prismadb'
 import {compare} from 'bcrypt'
 
+import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 export default NextAuth({
     providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || ''
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+        }),
         Credentials({
             id: 'credentials',
             name: 'Credentials',
@@ -44,6 +57,7 @@ export default NextAuth({
         signIn: '/auth',
     },
     debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt'
     },
