ee772ff44a52e44edcd4705c95524b00d0c09b70
Murilo Avila <murilo.avila068@gmail.com>
2023-05-14 01:11:24 -0300

    feat: protected routes, profiles screen and navbar

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -8,7 +8,6 @@ import {FcGoogle} from 'react-icons/fc'
 import {FaGithub} from 'react-icons/fa'
 
 const Auth = () => {
-    const router = useRouter()
     const [email, setEmail] = useState('')
     const [name, setName] = useState('')
     const [password, setPassword] = useState('')
@@ -21,14 +20,13 @@ const Auth = () => {
     const login = useCallback(async () => {
         try {
             await signIn('credentials', {
-                email, password, redirect: false, callbackUrl: '/'
+                email, password, callbackUrl: '/profiles'
             })
 
-            router.push('/')
         } catch (error) {
             console.log(error)
         }
-    }, [email, password, router])
+    }, [email, password])
     
     const register = useCallback(async () => {
         try {
@@ -86,7 +84,7 @@ const Auth = () => {
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                             <div
                                 onClick={() => signIn('google', {
-                                    callbackUrl: '/'
+                                    callbackUrl: '/profiles'
                                 })}
                                 className="
                                     w-10
@@ -105,7 +103,7 @@ const Auth = () => {
                             </div>
                             <div
                                 onClick={() => signIn('github', {
-                                    callbackUrl: '/'
+                                    callbackUrl: '/profiles'
                                 })}
                                 className="
                                     w-10
