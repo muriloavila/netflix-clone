483123f9e4e951e6ceb4b392d4da83cc439c614f
Murilo Avila <murilo.avila068@gmail.com>
2023-05-15 20:36:07 -0300

    fix nextAuth session, apply favorite button, MyList list

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BsFillPlayFill } from 'react-icons/bs'
+import FavoriteButton from './FavoriteButton'
 
 interface MovieCardProps {
     data: Record<string, any>
@@ -90,6 +91,7 @@ const MovieCard: React.FC<MovieCardProps> = ({data}) => {
                             >
                                 <BsFillPlayFill size={30}/>
                             </div>
+                            <FavoriteButton movieId={data?.id} />
                         </div>
 
                         <p className='text-green-400 font-bold mt-4'>
